a1c852632d5e16029b65173c40181a51d263bfe5
Down <53242610+Down-s@users.noreply.github.com>
2021-04-04 04:46:32 +0100

    Files

---
+++ main.lua
@@ -0,0 +1,30 @@
+local ball = require("ball")
+
+local tBalls = {}
+local BallCount = 200
+
+function love.load()
+    for i = 1, BallCount do
+        local ball = ball.Create()
+        ball:SetPos(math.random(0, love.graphics.getWidth()), math.random(0, love.graphics.getHeight()))
+        ball:SetRadius(math.random(32, 64))
+        ball:SetColor(math.random(), math.random(), math.random())
+
+        tBalls[i] = ball
+    end
+    
+    love.window.setTitle("Balls!")
+    love.graphics.setBackgroundColor(0.03, 0.03, 0.03)
+end
+
+function love.draw()
+    for i, ball in ipairs(tBalls) do
+        if (i == 25) then
+            local mx, my = love.mouse.getPosition()
+            ball:SetPos(mx, my)
+        end
+
+        ball:Render()
+        ball:PhysicsUpdate(tBalls)
+    end
+end
